0463c51b8ecb7bf56037687adba14efe3a096dd7
s225079 <s225079@student.dtu.dk>
2025-06-04 20:20:45 +0200

    Dot plot.R

--- Dot plot.R
+++ Dot plot.R
@@ -11,23 +11,20 @@ data <- read.delim("C:/Users/Miras/OneDrive - Danmarks Tekniske Universitet/Skri
 # Print the column names to check them
 print(colnames(data))
 
-# Assuming the first row contains the SCFA products and the first column contains the bacterial strains
 data_long <- data %>%
   pivot_longer(cols = -1, names_to = "Bacterial_Strain", values_to = "Presence") %>% filter(Presence == "+")  # Keep only rows where Presence is "+"
 
 
 
-print(data_long)
 
-
-# Create the dot plot with larger dots and adjusted axis text size
+# plot
 ggplot(data_long, aes(x = Bacterial_Strain, y = `SCFA_product`)) +
-  geom_point(size = 6) +  # Adjust the size value as needed
+  geom_point(size = 6) + 
   theme_minimal() +
   labs(title = " SCFA Products by Bacterial Strains",
        x = "Bacterial Strain",
        y = "SCFA Product") +
-  theme(axis.text = element_text(size = 26),  # Adjust the text size for axis labels
+  theme(axis.text = element_text(size = 26), 
         axis.title = element_text(size = 26),
         plot.title = element_text(size = 30, hjust = 0.5),
         axis.text.x = element_text(angle = 45, hjust = 1, face = "italic",color="black"),
