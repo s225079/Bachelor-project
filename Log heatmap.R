f13899dd60b91efd69834f9ab56d0a10c9b9a6c0
s225079 <s225079@student.dtu.dk>
2025-06-04 20:23:26 +0200

    Log heatmap.R

--- Log heatmap.R
+++ Log heatmap.R
@@ -14,7 +14,7 @@ df6 <- read.table(paste0(path_heat, "/B_long_family_substrate_counts.txt"), sep 
 df7 <- read.table(paste0(path_heat, "/P_clar_family_substrate_counts.txt"), sep = "\t", header = TRUE)
 df8 <- read.table(paste0(path_heat, "/I_but_family_substrate_counts.txt"), sep = "\t", header = TRUE)
 
-# Create a list of data frames with names for each strain
+# list of data frames with names for each strain
 strain_data <- list(
   'B. intestini' = df3,
   'S. salivae' = df2,
@@ -35,10 +35,7 @@ substrate_colors <- c(
   'fructan'='orange',
   'pectin' = 'lightcoral',
   'starch' = 'turquoise2',
-  'xylan' = 'seagreen3'
-  
-  # Add more substrates and their corresponding colors here
-)
+  'xylan' = 'seagreen3')
 
 # Combine all strains into a single data frame
 combined_data <- bind_rows(strain_data, .id = "Strain")
@@ -60,16 +57,16 @@ heatmap_data <- combined_data %>%
 
 # Convert to matrix
 heatmap_matrix <- as.matrix(heatmap_data %>% select(-RowID)) 
-rownames(heatmap_matrix) <- heatmap_data$RowID  # Set row names as "family_substrate"
+rownames(heatmap_matrix) <- heatmap_data$RowID  
 
-# Ensure columns are ordered correctly
+
 strain_order <- c("A. rectalis", "S. salivae", "B. intestini", "S. copri", "B. ovatus", "B. infantis", "P. clara", "I. butyriciproducens")
 heatmap_matrix <- heatmap_matrix[, strain_order, drop = FALSE]
 
 # Transpose matrix to flip rows and columns
 heatmap_matrix <- t(heatmap_matrix)
 
-# Log normalization (log1p to avoid log(0) issues)
+# Log normalization (log1p)
 heatmap_matrix_normalized <- log1p(heatmap_matrix)
 
 
@@ -94,30 +91,7 @@ strain_labels <- c("italic('A. rectalis')", "italic('S. salivae')", "italic('B. 
 # Convert strings to expressions
 strain_labels_expr <- parse(text = strain_labels)
 
-
-# Replace "beta"/"alpha" with Greek letters in substrate names
-annotation_row$Substrate <- gsub("beta-glucan", "β-glucan", annotation_row$Substrate)
-annotation_row$Substrate <- gsub("beta-mannan", "β-mannan", annotation_row$Substrate)
-annotation_row$Substrate <- gsub("beta-galactan", "β-galactan", annotation_row$Substrate)
-annotation_row$Substrate <- gsub("alpha-glucan", "α-glucan", annotation_row$Substrate)
-
-# Updated substrate_colors with Greek names
-substrate_colors <- c(
-  'α-glucan' = 'lightpink',
-  'arabinan' = 'orchid2',
-  'β-galactan' = 'olivedrab3',
-  'β-glucan' = 'mediumpurple1',
-  'β-mannan' = 'royalblue',
-  'fructan' = 'orange',
-  'pectin' = 'lightcoral',
-  'starch' = 'turquoise2',
-  'xylan' = 'seagreen3'
-)
-
-
-
-
-# Plot heatmap with updated annotation and color labels
+# heatmap 
 pheatmap(heatmap_matrix_normalized, cluster_rows = TRUE, cluster_cols = FALSE,
          main = "CAZyme families linked to dietary fibers identified in bacterial strains",
          angle_col = 90, fontsize=15,
@@ -133,43 +107,3 @@ pheatmap(heatmap_matrix_normalized, cluster_rows = TRUE, cluster_cols = FALSE,
 )
 
 
-library(grid)
-
-# Draw heatmap without title
-p <- pheatmap(
-  heatmap_matrix_normalized,
-  cluster_rows = TRUE,
-  cluster_cols = FALSE,
-  angle_col = 90,
-  fontsize=15,
-  annotation_col = annotation_row,
-  annotation_colors = list(Substrate = substrate_colors),
-  breaks = seq(min(heatmap_matrix_normalized), max(heatmap_matrix_normalized), length.out = 100),
-  show_rownames = TRUE, 
-  show_colnames = TRUE, 
-  border_color = NA,
-  labels_col = family_names,
-  labels_row = strain_labels_expr,
-  legend = TRUE
-)
-
-# Add title with custom size
-#grid::grid.text("CAZyme families linked to dietary fibers identified in bacterial strains",
- #               x = 0.5, y = 0.98, gp = gpar(fontsize = 26, fontface = "bold"))
-
-p <- pheatmap(
-  t(heatmap_matrix_normalized),  # transpose the matrix
-  cluster_rows = FALSE,          # previously columns
-  cluster_cols = TRUE,           # previously rows
-  angle_col = 90,
-  fontsize = 15,
-  annotation_row = annotation_row,  # was annotation_col
-  annotation_colors = list(Substrate = substrate_colors),
-  breaks = seq(min(heatmap_matrix_normalized), max(heatmap_matrix_normalized), length.out = 100),
-  show_rownames = TRUE, 
-  show_colnames = TRUE, 
-  border_color = NA,
-  labels_row = family_names,         # used to be labels_col
-  labels_col = strain_labels_expr,   # used to be labels_row
-  legend = TRUE
-)
